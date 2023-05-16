851f1696bb7732664e10a4a24a58d3ce66164306
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-05-15 17:36:16 -0700

    Add player

--- Scene1.js
+++ Scene1.js
@@ -19,11 +19,53 @@ class Scene1 extends Phaser.Scene {
             frameWidth: 16,
             frameHeight: 16
         })
+        this.load.spritesheet("player", "assets/spritesheets/player.png", {
+            frameWidth: 16,
+            frameHeight: 24
+        })
     }
 
     create() {
         this.add.text(20, 20, "Loading game...")
 
+        this.anims.create({
+            key: "ship1_anim",
+            frames: this.anims.generateFrameNumbers("ship"),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "explode",
+            frames: this.anims.generateFrameNumbers("explosion"),
+            frameRate: 20,
+            repeat: 0,
+            hideOnComplete: false
+        })
+
+        this.anims.create({
+            key: "red",
+            frames: this.anims.generateFrameNumbers("power-up", {
+                start: 0,
+                end: 1
+            }),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "gray",
+            frames: this.anims.generateFrameNumbers("power-up", {
+                start: 2,
+                end: 3
+            }),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "thrust",
+            frames: this.anims.generateFrameNumbers("player"),
+            frameRate: 20,
+            repeat: -1
+        })
         this.scene.start("playGame")
     }
 }
--- Scene2.js
+++ Scene2.js
@@ -10,19 +10,8 @@ class Scene2 extends Phaser.Scene {
         this.ship1 = this.add.sprite(config.width / 2 - 50, config.height / 2, "ship")
         this.ship1.setScale(2)
         this.ship1.flipY = true
-        this.anims.create({
-            key: "ship1_anim",
-            frames: this.anims.generateFrameNumbers("ship"),
-            frameRate: 20,
-            repeat: -1
-        })
-        this.anims.create({
-            key: "explode",
-            frames: this.anims.generateFrameNumbers("explosion"),
-            frameRate: 20,
-            repeat: 0,
-            hideOnComplete: false
-        })
+
+
         this.ship1.play("ship1_anim")
         this.ship1.setInteractive()
         this.input.on("gameobjectdown", this.destroyShip, this)
@@ -30,27 +19,6 @@ class Scene2 extends Phaser.Scene {
 
         this.ship2 = this.add.image(config.width / 2, config.height / 2, "ship2")
         this.ship3 = this.add.image(config.width / 2 + 50, config.height / 2, "ship3")
-
-        this.add.text(20, 20, "Playing Game", {font:"25px Arial", fill: "yellow"})
-
-        this.anims.create({
-            key: "red",
-            frames: this.anims.generateFrameNumbers("power-up", {
-                start: 0,
-                end: 1
-            }),
-            frameRate: 20,
-            repeat: -1
-        })
-        this.anims.create({
-            key: "gray",
-            frames: this.anims.generateFrameNumbers("power-up", {
-                start: 2,
-                end: 3
-            }),
-            frameRate: 20,
-            repeat: -1
-        })
 
         this.powerUps = this.physics.add.group()
         const maxObjects = 4
@@ -69,6 +37,14 @@ class Scene2 extends Phaser.Scene {
             powerUp.setCollideWorldBounds(true)
             powerUp.setBounce(1.0)
         }
+
+        this.player = this.physics.add.sprite(config.width/2-8, config.height-64, "player")
+        this.player.setOrigin(0, 0)
+        this.player.setCollideWorldBounds(true)
+        this.player.play("thrust")
+
+        this.cursorKeys = this.input.keyboard.createCursorKeys()
+        this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
     }
 
     update() {
@@ -78,6 +54,12 @@ class Scene2 extends Phaser.Scene {
         this.moveShip(this.ship3, 3)
 
         this.background.tilePositionY -= 0.5
+
+        this.movePlayerManager()
+
+        if(Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+            console.log("Fire!")
+        }
     }
 
     moveShip(ship, speed) {
@@ -101,5 +83,19 @@ class Scene2 extends Phaser.Scene {
 
     restart() {
         this.ship1.play("ship1_anim")
+    }
+
+    movePlayerManager() {
+        if(this.cursorKeys.left.isDown) {
+            this.player.setVelocityX(-gameSettings.playerSpeed)
+        } else if(this.cursorKeys.right.isDown) {
+            this.player.setVelocityX(gameSettings.playerSpeed)
+        }
+
+        if(this.cursorKeys.up.isDown) {
+            this.player.setVelocityY(-gameSettings.playerSpeed)
+        } else if(this.cursorKeys.down.isDown) {
+            this.player.setVelocityY(gameSettings.playerSpeed)
+        }
     }
 }
